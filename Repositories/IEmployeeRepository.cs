5eed1a310e614d433b6a583147da4746e56636b9
Daniel Bojchovski <106997266+DanielBojchovski@users.noreply.github.com>
2022-07-08 15:15:29 +0200

    Create IEmployeeRepository.cs

---
+++ Repositories/IEmployeeRepository.cs
@@ -0,0 +1,33 @@
+using ApiEmployee6.Models;
+
+namespace ApiEmployee6.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        //COMMANDS
+        Task<Employee> Create(Employee employee);
+        Task<Employee> Update(Employee employee);
+        Task Delete(int employeeId);
+
+        //QUERIES
+        Task<IEnumerable<Employee>> Get();
+        Task<Employee> Get(int employeeId);
+        Task<Employee> GetEmployeeByEmail(string email);
+        Task<IEnumerable<Employee>> SearchByName(string name);
+        Task<IEnumerable<Employee>> SearchBySkill(string skill);
+        double GetAverageSalary();
+        double GetTotalSumOfSalaries();
+        Task<IEnumerable<Employee>> GetSalariesBiggerThen(double salary);
+        Task<IEnumerable<Employee>> GetSalariesLessThen(double salary);
+        Task<IEnumerable<Employee>> GetSalariesInRange(double startPoint, double endPoint);
+        Task<IEnumerable<Employee>> GetAgeLessThen(int age);
+        Task<IEnumerable<Employee>> GetAgeBiggerThen(int age);
+        Task<IEnumerable<Employee>> GetAgeInRange(int startPoint, int endPoint);
+        Task<IEnumerable<Employee>> SearchByDepartmentName(string departmentName);
+        Task<IEnumerable<Employee>> SearchByGender(string gender);
+        Task<IEnumerable<Employee>> OrderByAgeDescending();
+        Task<IEnumerable<Employee>> OrderByAgeAscending();
+        Task<IEnumerable<Employee>> OrderBySalaryDescending();
+        Task<IEnumerable<Employee>> OrderBySalaryAscending();
+    }
+}
